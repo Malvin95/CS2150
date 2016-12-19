cc532e50f30a18f501180f71f3eadb8e78940908
Malvin95 <Malvinharding@hotmail.co.uk>
2016-12-19 19:17:31 +0000

    Update ShapeDesigner.java

--- CS2150/CS2150_Workspace/CS2150/Designer/ShapeDesigner.java
+++ CS2150/CS2150_Workspace/CS2150/Designer/ShapeDesigner.java
@@ -36,29 +36,77 @@ public class ShapeDesigner extends AbstractDesigner {
 	/** Draw the shape **/
     protected void drawUnitShape()
     {
-    	Vertex v1 = new Vertex(-0.5f, 0.5f, 0.5f);
-		Vertex v2 = new Vertex(0.0f, 1.0f, 0.5f);
-		Vertex v3 = new Vertex(0.5f, 0.5f, 0.5f);
-		Vertex v4 = new Vertex(-0.5f, 0.5f, -0.5f);
-		Vertex v5 = new Vertex(0.5f, 0.5f, -0.5f);
-		Vertex v6 = new Vertex(0.0f, 1.0f, -0.5f);
+    	Vertex v1 = new Vertex(-0.5f, 0.0f, 1.0f);
+		Vertex v2 = new Vertex(0.5f, 0.0f, 1.0f);
+		Vertex v3 = new Vertex(0.5f, 0.0f, -1.0f);
+		Vertex v4 = new Vertex(-0.5f, 0.0f, -1.0f);
 		
-		/* GL11.glBegin(GL11.GL_POINTS);
+		/*
+		Vertex v5 = new Vertex(0.0f, 0.0f, 0.0f);
+		Vertex v6 = new Vertex(0.0f, 1.0f, 0.0f);
+		Vertex v7 = new Vertex(1.0f, 1.0f, 0.0f);
+		Vertex v8 = new Vertex(1.0f, 0.0f, 0.0f);
 		
-		v1.submit();
-		v2.submit();
-		v3.submit();
-		v4.submit();
-		v5.submit();
-		v6.submit();
+		Vertex v9 = new Vertex(0.25f, 0.25f, 2.5f);
+		Vertex v10 = new Vertex(0.25f, 0.75f, 2.5f);
+		Vertex v11 = new Vertex(0.75f, 0.75f, 2.5f);
+		Vertex v12 = new Vertex(0.75f, 0.25f, 2.5f);
+		Vertex v13 = new Vertex(0.25f, 0.25f, 2.0f);
+		Vertex v14 = new Vertex(0.25f, 0.75f, 2.0f);
+		Vertex v15 = new Vertex(0.75f, 0.75f, 2.0f);
+		Vertex v16 = new Vertex(0.75f, 0.25f, 2.0f);
+
+		Vertex v17 = new Vertex(0.375f, 0.375f, 2.75f);
+		Vertex v18 = new Vertex(0.375f, 0.625f, 2.75f);
+		Vertex v19 = new Vertex(0.625f, 0.625f, 2.75f);
+		Vertex v20 = new Vertex(0.625f, 0.375f, 2.75f);
+		Vertex v21 = new Vertex(0.375f, 0.375f, 2.5f);
+		Vertex v22 = new Vertex(0.375f, 0.625f, 2.5f);
+		Vertex v23 = new Vertex(0.625f, 0.625f, 2.5f);
+		Vertex v24 = new Vertex(0.625f, 0.375f, 2.5f);
+		*/
 		
-		GL11.glEnd(); */
+		GL11.glBegin(GL11.GL_POINTS);
+		
+			v1.submit();
+			v2.submit();
+			v3.submit();
+			v4.submit();
+
+			GL11.glEnd();
+			
+			/*
+			v5.submit();
+			v6.submit();
+			v7.submit();
+			v8.submit();
+			
+			v9.submit();
+			v10.submit();
+			v11.submit();
+			v12.submit();
+			v13.submit();
+			v14.submit();
+			v15.submit();
+			v16.submit();
+			
+			v17.submit();
+			v18.submit();
+			v19.submit();
+			v20.submit();
+			v21.submit();
+			v22.submit();
+			v23.submit();
+			v24.submit();
+		
+		GL11.glEnd();
 		
 		GL11.glBegin(GL11.GL_POLYGON);
         {
         	submitNextColour();
 			//Near face
 			
+            v4.submit();
             v3.submit();
             v2.submit();
             v1.submit();
@@ -70,9 +118,22 @@ public class ShapeDesigner extends AbstractDesigner {
         	submitNextColour();
         	//Far face
         	
-            v4.submit();
+        	v5.submit();
             v6.submit();
-            v5.submit();
+            v7.submit();
+            v8.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Top face
+
+            v7.submit();
+            v6.submit();
+            v2.submit();
+            v3.submit();
         }
         GL11.glEnd();
         
@@ -82,9 +143,9 @@ public class ShapeDesigner extends AbstractDesigner {
             //Bottom face
 
             v1.submit();
-            v4.submit();
             v5.submit();
-            v3.submit();
+            v8.submit();
+            v4.submit();
         }
         GL11.glEnd();
         
@@ -93,24 +154,177 @@ public class ShapeDesigner extends AbstractDesigner {
         	submitNextColour();
             //Left face
 
+            v1.submit();
             v2.submit();
             v6.submit();
-            v4.submit();
-            v1.submit();
+            v5.submit();
         }
         GL11.glEnd();
-        
         
         GL11.glBegin(GL11.GL_POLYGON);
         {
         	submitNextColour();
             //Right face
 
-            v6.submit();
-            v2.submit();
+            v8.submit();
+            v7.submit();
             v3.submit();
-            v5.submit();
+            v4.submit();
         }
         GL11.glEnd();
+        
+        /*
+         * 
+         * LARGER LENS PIECE
+         * 
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+			//Lens Near face
+			
+            v12.submit();
+            v11.submit();
+            v10.submit();
+            v9.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+        	//Lens Far face
+        	
+        	v13.submit();
+            v14.submit();
+            v15.submit();
+            v16.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Top face
+
+            v15.submit();
+            v14.submit();
+            v10.submit();
+            v11.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Bottom face
+
+            v9.submit();
+            v13.submit();
+            v16.submit();
+            v12.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Left face
+
+            v9.submit();
+            v10.submit();
+            v14.submit();
+            v13.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Right face
+
+            v16.submit();
+            v15.submit();
+            v11.submit();
+            v12.submit();
+        }
+        GL11.glEnd();
+        
+        /*
+         * 
+         * SMALLER LENS PIECE
+         * 
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+			//Lens Near face
+			
+            v20.submit();
+            v19.submit();
+            v18.submit();
+            v17.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+        	//Lens Far face
+        	
+        	v21.submit();
+            v22.submit();
+            v23.submit();
+            v24.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Top face
+
+            v23.submit();
+            v22.submit();
+            v18.submit();
+            v19.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Bottom face
+
+            v17.submit();
+            v21.submit();
+            v24.submit();
+            v20.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Left face
+
+            v17.submit();
+            v18.submit();
+            v22.submit();
+            v21.submit();
+        }
+        GL11.glEnd();
+        
+        GL11.glBegin(GL11.GL_POLYGON);
+        {
+        	submitNextColour();
+            //Lens Right face
+
+            v24.submit();
+            v23.submit();
+            v19.submit();
+            v20.submit();
+        }
+        GL11.glEnd(); 
+        */
+        
     }
 }
